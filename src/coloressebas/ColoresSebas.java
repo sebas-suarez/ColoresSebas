1f04f44275dfd9422b681d315f13ef5277eb3ad8
marcoezequiel92 <marcoezequielsuarez92@gmail.com>
2019-11-19 20:53:15 -0300

    Cambio de colores favoritos

--- src/coloressebas/ColoresSebas.java
+++ src/coloressebas/ColoresSebas.java
@@ -10,7 +10,7 @@ public class ColoresSebas {
         String color1, color2, color3;
         color1 = "AZUL";
         color2 = "ROJO";
-        color3 = "VERDE";
+        color3 = "VIOLETA";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n",color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
