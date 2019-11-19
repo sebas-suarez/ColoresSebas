5aa85cf314cbd5527a61f6236decced0f3211cae
sebas-suarez <sebasuarez725@gamil.com>
2019-11-19 20:39:27 -0300

    primera version

---
+++ src/coloressebas/ColoresSebas.java
@@ -0,0 +1,20 @@
+
+package coloressebas;
+
+
+public class ColoresSebas {
+
+
+    public static void main(String[] args) {
+    
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VERDE";
+        System.out.printf("%s es mi primer color favorito\n", color1);
+        System.out.printf("%s es mi segundo color favorito\n",color2);
+        System.out.printf("%s es mi tercer color favorito\n", color3);
+        
+    }
+    
+}
