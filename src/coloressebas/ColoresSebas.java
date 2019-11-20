2cecee05ebd8197c9a9a75bacb1fb0822b5e12c1
sebas-suarez <sebasuarez725@gamil.com>
2019-11-19 21:06:33 -0300

    se actualiza la lista de colores

--- src/coloressebas/ColoresSebas.java
+++ src/coloressebas/ColoresSebas.java
@@ -7,13 +7,15 @@ public class ColoresSebas {
 
     public static void main(String[] args) {
     
-        String color1, color2, color3;
+        String color1, color2, color3, color4;
         color1 = "AZUL";
         color2 = "ROJO";
         color3 = "VERDE";
+        color4 = "MAGENTA";
         System.out.printf("%s es mi primer color favorito\n", color1);
-        System.out.printf("%s es mi segundo color favorito\n",color2);
+        System.out.printf("%s no es mi color favorito pero tambien me gusta\n",color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
+        System.out.printf("%s es el ultimo color de la lista\n", color4);
         
     }
     
